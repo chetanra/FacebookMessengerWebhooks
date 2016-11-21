b40d95152352f745e7e8878413522cf4c70f45b8
chetanra <chetan_pr@outlook.com>
2016-11-21 08:47:04 -0800

    Added new test case for url button
    to verify WebviewHeightRatio isn't set by default.

--- FBMessenger.Tests/ButtonTests.cs
+++ FBMessenger.Tests/ButtonTests.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Facebook.Messenger;
 using Facebook.Messenger.Buttons;
 using Facebook.Messenger.Postbody.Buttons;
@@ -10,7 +10,7 @@ namespace FBMessenger.Tests
     public class ButtonTests
     {
         [TestMethod]
-        public void UrlButton()
+        public void UrlButton1()
         {
             var b = new UrlButtons();
             b.Recipient.Id = "USER_ID";
@@ -18,6 +18,18 @@ namespace FBMessenger.Tests
             b.Message.Attachment.Payload.Elements[0].Buttons.Add(new UrlButton {Url="https://tshirt", Title="view item", WebviewHeightRatio=WebviewHeightRatioEnum.tall });
             var str = JsonConvert.SerializeObject(b, new JsonSerializerSettings {NullValueHandling = NullValueHandling.Ignore,   });
             var expected = "{\"message\":{\"attachment\":{\"type\":\"template\",\"payload\":{\"elements\":[{\"title\":\"Classic White T-Shirt\",\"item_url\":\"https://tshirt\",\"image_url\":\"https://tshirt.png\",\"subtitle\":\"soft white\",\"buttons\":[{\"webview_height_ratio\":\"tall\",\"url\":\"https://tshirt\",\"title\":\"view item\",\"type\":\"web_url\"}]}],\"template_type\":\"generic\"}}},\"recipient\":{\"id\":\"USER_ID\"}}";
+            Assert.AreEqual(str, expected);
+        }
+
+        [TestMethod]
+        public void UrlButton2()
+        {
+            var b = new UrlButtons();
+            b.Recipient.Id = "USER_ID";
+            b.Message.Attachment.Payload.Elements.Add(new Element<UrlButton> { Title = "Classic White T-Shirt", ItemUrl = "https://tshirt", ImageUrl = "https://tshirt.png", Subtitle = "soft white" });
+            b.Message.Attachment.Payload.Elements[0].Buttons.Add(new UrlButton { Url = "https://tshirt", Title = "view item"});
+            var str = JsonConvert.SerializeObject(b, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore, });
+            var expected = "{\"message\":{\"attachment\":{\"type\":\"template\",\"payload\":{\"elements\":[{\"title\":\"Classic White T-Shirt\",\"item_url\":\"https://tshirt\",\"image_url\":\"https://tshirt.png\",\"subtitle\":\"soft white\",\"buttons\":[{\"url\":\"https://tshirt\",\"title\":\"view item\",\"type\":\"web_url\"}]}],\"template_type\":\"generic\"}}},\"recipient\":{\"id\":\"USER_ID\"}}";
             Assert.AreEqual(str, expected);
         }
     }
